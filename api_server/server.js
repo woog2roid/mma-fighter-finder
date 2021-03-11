3106d6b25b246bee2edb7d5eaab7527814343d6a
woog2roid <wooguijung@korea.ac.kr>
2021-03-11 23:40:34 +0900

    api server <-> frontend connection complete

--- api_server/server.js
+++ api_server/server.js
@@ -3,8 +3,6 @@
     (This version is not on the npm...)
     - npm google module -> npm google-it module
     - remove ufc module...
-
-    server.js code from here: "https://github.com/styfle/mma-api"
 */
 
 const { createServer } = require('http');
@@ -13,40 +11,39 @@ const { fighter } = require("mma");
 
 const hostname = '127.0.0.1';
 const port = 3000;
-
 let cache = new Map();
 
 const server = createServer((req, res) => {
+    // URL = "127.0.0.1:3000?fighter=jon%20jones"
     let { httpVersion, method, url } = req;
     console.log(`${httpVersion} ${method} ${url}`);
     let { query = {} } = parse(url || '', true);
+    
+    //CORS
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Content-Type', 'application/json');
 
-    // URL = "127.0.0.1:3000?fighter=jon%20jones"
-    if (!query.fighter) {
-        const data = { error: 'Fighter Not Found' };
-        res.statusCode = 400;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(data, null, ' '));
-        return;
-    }
-
-    if (cache.has(query.fighter)) {
-        const data = cache.get(query.fighter);
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(data, null, ' '));
-        return;
-    }
-
-    fighter(query.fighter, data => {
-        cache.set(query.fighter, data);
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(data, null, ' '));
+    fighter(query.fighter, info => {
+        cache.set(query.fighter, info);
+        if (!query.fighter) { // URL이 잘못됨
+            res.statusCode = 400;
+            res.end();
+            return;
+        } else if (cache.has(query.fighter)) {
+            const data = cache.get(query.fighter);
+            if (!data) { //해당 선수 없음
+                res.statusCode = 404;
+                res.end();
+                return;
+            } else { // 정상적인 경우
+                res.statusCode = 200;
+                res.end(JSON.stringify(data, null, ' '));
+                return;
+            }
+        }
     });
 });
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
-
--- src/components/search/SearchContents.js
+++ src/components/search/SearchContents.js
@@ -1,18 +1,58 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import qs from 'qs';
+import axios from 'axios';
 import styled from 'styled-components';
 import { ColorCollection } from '../../utils/ColorCollection';
 import { FontWeight } from '../../utils/FontWeight';
 
+const LoadingWrapper = styled.div`
+    color: white;
+`;
+const ErrorWrapper = styled.div`
+    color: white;
+`;
+const Wrapper = styled.div`
+    color: white;
+`;
+
+
 const SearchContents = ({ location }) => {
-    const query = qs.parse(location.search, {
-    ignoreQueryPrefix: true
-    });
-    const fighter = query.fighter;
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    //qs + axios로 api server에서 data 받아오기
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setLoading(true);
+                const query = qs.parse(location.search, {
+                    ignoreQueryPrefix: true
+                });
+                const fighter = query.fighter;
+                const response = await axios.get(`http://127.0.0.1:3000?fighter=${fighter}`);
+                setData(response);
+            } catch (e) {
+                console.log(e);
+            }
+            setLoading(false);
+        }
+        fetchData();
+    }, []);
     
-    return (
-        <div>
-        </div>
+    if (loading) { // 아직 로딩 중인 경우
+        return (<LoadingWrapper>Loading...</LoadingWrapper>);
+    } else if (data === null) { // 선수가 없는 경우...
+        alert(data);
+        return (
+            <ErrorWrapper>
+                vf
+            </ErrorWrapper>
+        );
+    }
+    return ( // 정상적으로 내보내야 하는 경우...
+        <Wrapper>
+            hi there?
+        </Wrapper>
     );
 };
 
