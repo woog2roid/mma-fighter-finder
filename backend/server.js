c298535fd298c5d078d24ed6606839f135e14694
Woo <54667577+woog2roid@users.noreply.github.com>
2021-05-06 00:15:41 +0900

    fix(backend/server.js): hostname삭제

--- backend/server.js
+++ backend/server.js
@@ -43,6 +43,6 @@ const server = createServer((req, res) => {
     });
 });
 
-server.listen(port, hostname, () => {
+server.listen(port, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
