7acf3209eb167660b2929e3a95004472ac64c23d
woog2roid <woog2roid@gmail.com>
2022-11-14 05:55:02 +0000

    서비스 운영 종료 페이지 작성

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Footer from '../components/common/Footer';
 import Nav from '../components/common/Nav';
+import OutOfService from '../components/main/OutOfService';
 import Search from '../components/main/Search';
 
 const Main = () => {
     return (
         <>
             <Nav />
-            <Search />
+            {/*<Search /> */}
+            <OutOfService />
             <Footer />
         </>
     );
